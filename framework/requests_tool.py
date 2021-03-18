61a4a848b2658fc7412ccf882c989889d41683ba
letuwwa <letuwwa@gmail.com>
2021-03-18 13:54:02 +0300

    commit #2

--- framework/requests_tool.py
+++ framework/requests_tool.py
@@ -1,13 +1,24 @@
 import requests
+import re
 
 
 class RequestsTool:
     def __init__(self):
         self.headers = {"User-Agent": "Chrome/89.0.4389.90"}
 
-        self.base_url = 'http://jobs.tut.by'
+        self.base_url = 'https://rabota.by'
         self.search_url_addon = '/search/vacancy?area=1002&fromSearchLine=true&st=searchVacancy&text='
+        self.page_addon = '&page='
+        self.key_word = None
 
+    def search_by_key_word(self, key_word, page_number=None):
+        self.key_word = key_word
+        if page_number is None:
+            return requests.get(self.base_url + self.search_url_addon + self.key_word, headers=self.headers)
+        else:
+            return requests.get(self.base_url + self.search_url_addon + self.key_word +
+                                self.page_addon + str(page_number - 1), headers=self.headers)
 
-    def search_by_key_word(self, key_word):
-        return requests.get(self.base_url + self.search_url_addon + key_word, headers=self.headers)
+    def get_links_to_vacancies(self, text):
+        return re.findall(self.base_url + r'/vacancy/\d+', text)
+
--- tests.py
+++ tests.py
@@ -3,5 +3,14 @@ from framework.requests_tool import RequestsTool
 
 def test_one():
     actions = RequestsTool()
-    result = actions.search_by_key_word('python')
-    print(result.text)
+    links_list = []
+    page_count = 5
+
+    for i in range(page_count):
+        response = actions.search_by_key_word('python', page_number=i)
+        page_links = actions.get_links_to_vacancies(response.text)
+        links_list.extend(page_links)
+
+    for link in links_list:
+        print(link)
+    print(len(links_list))
